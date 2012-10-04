03a9950179f3460e4149bcc7b44a7cbb3b45fe08
badbod99@gmail.com <badbod99@users.noreply.github.com>
2012-10-04 16:38:47 +0000

    Refactored Mouse Simulation Added Reflection -> delegate based .touches calling.

--- Assets/Plugins/MindstormGestures/Config/TouchHandlerConfig.cs
+++ Assets/Plugins/MindstormGestures/Config/TouchHandlerConfig.cs
@@ -1,39 +1,93 @@
+/*
+Unity3d-TUIO connects touch tracking from a TUIO to objects in Unity3d.
+
+Copyright 2011 - Mindstorm Limited (reg. 05071596)
+
+Author - Simon Lerpiniere
+
+This file is part of Unity3d-TUIO.
+
+Unity3d-TUIO is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Unity3d-TUIO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser Public License for more details.
+
+You should have received a copy of the GNU Lesser Public License
+along with Unity3d-TUIO.  If not, see <http://www.gnu.org/licenses/>.
+
+If you have any questions regarding this library, or would like to purchase 
+a commercial licence, please contact Mindstorm via www.mindstorm.com.
+*/
+
 using UnityEngine;
+using System;
+using System.Reflection;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Mindstorm.Gesture.Config
 {
 	[System.Serializable]
 	public class TouchHandlerConfig
 	{
-		public InputTypeEnum InputType = InputTypeEnum.Native;
-		
 		public enum InputTypeEnum
 		{
-			Native = 0,
-			Tuio = 1,
-			Mouse = 2
+			Native = 1,
+			Tuio = 2
+		}
+		
+		Func<Touch[]> touchGetter;
+		
+		public InputTypeEnum InputType = InputTypeEnum.Native;
+		InputTypeEnum OldInputType = InputTypeEnum.Native;
+
+		Dictionary<InputTypeEnum, InputTypeMethod> InputTypes = new Dictionary<InputTypeEnum, InputTypeMethod>();
+				
+		public TouchHandlerConfig()
+		{
+			InputTypes.Add(InputTypeEnum.Native, new InputTypeMethod("UnityEngine", "UnityEngine.Input", "touches"));
+			InputTypes.Add(InputTypeEnum.Tuio, new InputTypeMethod("Assembly-CSharp-firstpass", "TuioInput", "touches"));
+			OldInputType = InputType;
 		}
 		
 		public Touch[] GetTouches()
-		{
-			Touch[] allTouches;
-			switch (InputType)
+		{	
+			if (touchGetter == null || InputType != OldInputType) 
 			{
-			case InputTypeEnum.Native:
-				allTouches = Input.touches;
-				break;
-			case InputTypeEnum.Tuio:
-				allTouches = TuioInput.touches;
-				break;
-			case InputTypeEnum.Mouse:
-				allTouches = MouseInput.touches;
-				break;
-			default:
-				allTouches = new Touch[0];
-				break;
+				OldInputType = InputType;
+				InitGetter();
 			}
+			
+			Touch[] allTouches = touchGetter();
 			return allTouches;
+		}
+		
+		void InitGetter()
+		{
+			Assembly b = Assembly.Load(InputTypes[InputType].AssemblyName);
+			Type t = b.GetType(InputTypes[InputType].ObjectName, true);
+			PropertyInfo p = t.GetProperty(InputTypes[InputType].PropertyName);
+			
+			touchGetter = (Func<Touch[]>)Delegate.CreateDelegate(typeof(Func<Touch[]>), p.GetGetMethod());
+		}
+	}
+	
+	public struct InputTypeMethod
+	{
+		public string AssemblyName;
+		public string ObjectName;
+		public string PropertyName;
+		
+		public InputTypeMethod(string assembly, string objectName, string property)
+		{
+			AssemblyName = assembly;
+			ObjectName = objectName;
+			PropertyName = property;
 		}
 	}
 }
--- Assets/Plugins/MindstormTuio/TouchComponents/TuioInput.cs
+++ Assets/Plugins/MindstormTuio/TouchComponents/TuioInput.cs
@@ -1,3 +1,29 @@
+/*
+Unity3d-TUIO connects touch tracking from a TUIO to objects in Unity3d.
+
+Copyright 2011 - Mindstorm Limited (reg. 05071596)
+
+Author - Simon Lerpiniere
+
+This file is part of Unity3d-TUIO.
+
+Unity3d-TUIO is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Unity3d-TUIO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser Public License for more details.
+
+You should have received a copy of the GNU Lesser Public License
+along with Unity3d-TUIO.  If not, see <http://www.gnu.org/licenses/>.
+
+If you have any questions regarding this library, or would like to purchase 
+a commercial licence, please contact Mindstorm via www.mindstorm.com.
+*/
+
 using System.Collections;
 using UnityEngine;
 using System.Linq;
@@ -5,32 +31,60 @@ using System.Linq;
 public class TuioInput : MonoBehaviour
 {
 	static TuioComponentBase tracking;
+	static TuioComponentBase mouseSim;
+	
+	static Touch[] frameTouches = new Touch[0];
+	
+	public bool SimulateFromMouse = false;
+	
+	public static int touchCount
+	{
+		get;
+		private set;
+	}
 	
 	void Awake()
 	{
-		tracking = new TuioTrackingComponent();
-		
-		tracking.ScreenWidth = Camera.main.pixelWidth;
-		tracking.ScreenHeight = Camera.main.pixelHeight;
+		tracking = InitTracking(new TuioTrackingComponent());
+		mouseSim = InitTracking(new MouseTrackingComponent());
 	}
 	
 	void Update()
 	{
-		tracking.BuildTouchDictionary();
+		TuioComponentBase tr = SimulateFromMouse ? mouseSim : tracking;
+		UpdateTouches(tr);
+	}
+	
+	void UpdateTouches(TuioComponentBase tr)
+	{
+		tr.BuildTouchDictionary();
+		frameTouches = tr.AllTouches.Values.Select(t => t.ToUnityTouch()).ToArray();
+		touchCount = frameTouches.Length;
+	}
+	
+	TuioComponentBase InitTracking(TuioComponentBase tr)
+	{
+		tr.ScreenWidth = Camera.main.pixelWidth;
+		tr.ScreenHeight = Camera.main.pixelHeight;
+		return tr;
+	}
+	
+	public static Touch GetTouch(int index)
+	{
+		return frameTouches[index];		
 	}
 	
 	public static Touch[] touches
 	{
 		get
 		{
-			if (tracking == null) return new Touch[0];
-			Touch[] uTouch = tracking.AllTouches.Values.Select(t => t.ToUnityTouch()).ToArray();
-			return uTouch;
+			return frameTouches;
 		}
 	}
 	
 	void OnApplicationQuit()
 	{
 		if (tracking != null) tracking.Close();
+		if (mouseSim != null) mouseSim.Close();
 	}
 }
